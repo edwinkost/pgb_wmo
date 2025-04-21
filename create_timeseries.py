bc3d0859335fd33f01531d311a410e975043e9d7
Edwin Sutanudjaja <edwinkost@UU105378>
2025-04-21 08:56:28 +0200

    rerun

--- create_timeseries.py
+++ create_timeseries.py
@@ -124,13 +124,16 @@ for irow in range(len(wmo_station_table)):
     model_lat, valid                    = pcr.cellvalue(pcr.mapminimum(pcr.ifthen(wmo_id_point, ycoord)        ), 1)
     model_area_km2_this_station, valid  = pcr.cellvalue(pcr.mapminimum(pcr.ifthen(wmo_id_point, model_area_km2)), 1)
     
-    # calculate area_deviation
+    # calculate area_deviation, do not use it if the deviation is large
+    use_this_station = True
     area_deviation = (model_area_km2_this_station - wmo_area_km2) / wmo_area_km2
+    if abs(area_deviation) > 100: use_this_station = False
     
     # - check where the mask for this
     mask_for_this_station, valid = pcr.cellvalue(pcr.mapmaximum(pcr.scalar(pcr.ifthen(wmo_id_point, mask))), 1)
+    if mask_for_this_station < 0: use_this_station = False
 
-    if abs(area_deviation) < 100. and mask_for_this_station < 100.:
+    if use_this_station:
 
     # ~ use_all = True
     # ~ if use_all:
